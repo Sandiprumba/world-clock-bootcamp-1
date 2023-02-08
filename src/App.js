6cb33a9260d099a099db07dafec7ec2bf0da3f97
Sandiprumba <sandiprumba.lama@gmail.com>
2023-02-08 18:00:40 +0800

    adding clock component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
 import React from "react";
 import logo from "./logo.png";
 import "./App.css";
+import Clock from "./Clock.js";
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            <img src={logo} className="App-logo" alt="logo" />{" "}
           </p>
+          <Clock timeZone="Asia/Singapore" /> <br></br>
+          <Clock timeZone="America/Los_Angeles" />
+          <br></br>
+          <Clock timeZone="Europe/London" />
         </header>
       </div>
     );
   }
 }
-
 export default App;
